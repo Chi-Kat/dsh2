3a63ee942efcbc309514c89846d987dd66332165
Shaan Shah <shahshaan@gmail.com>
2012-10-04 02:30:44 -0500

    removing before_create logic in classroom model and adding a default image 'backgound.jpg' for the image_uploader

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,6 +1,6 @@
 class Classroom < ActiveRecord::Base
   attr_accessible :name, :region_id, :stat, :description, :image, :map
-  before_save :add_images
+ 
   belongs_to :region
   has_many :attachments
   has_many :facilitators
@@ -8,12 +8,4 @@ class Classroom < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   mount_uploader :map, MapUploader
 
-  def add_images
-  	if self.image?
-  	else self.image = "https://digitalschoolhouse.s3.amazonaws.com/uploads/classroom/image/6/Screen_Shot_2012-10-03_at_4.53.04_PM.png"
-  	end
-  	if self.map?
-  	else self.map = "https://digitalschoolhouse.s3.amazonaws.com/uploads/classroom/image/6/Screen_Shot_2012-10-03_at_4.53.04_PM.png"
-  	end
-  end
 end

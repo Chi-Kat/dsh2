862e05701f0f01ef62173a21c95a9b1769c8cf4c
Shaan Shah <shahshaan@gmail.com>
2012-10-05 13:57:37 -0500

    adding validations for adding attachment, fixing broken youtube_url calls for attachment model, fixing home page

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -6,16 +6,43 @@ class Attachment < ActiveRecord::Base
   belongs_to :user
   belongs_to :region
   before_create :set_region
-
   mount_uploader :image, ImageUploader
+  validates_presence_of :image, :if => :youtube_not_present?, :message => "no go amigo"
+
+  def youtube_not_present?
+    domain = youtube_url.split('/')[2]
+    if youtube_url == "" || domain == nil
+      return true
+    elsif domain.include?("youtube") || domain.include?("youtu.be")
+      return false
+    else return true
+    end
+  end
 
  	def set_region
  		self.region_id = Classroom.find(self.classroom_id).region_id
  	end
 
+  def format?
+    if self.youtube_url.present?
+      link = self.youtube_url
+      domain = link.split('/')[2]
+      if domain != nil
+        if domain.include?("youtube") || domain.include?("youtu.be")
+          return "youtube"
+        end
+      end
+    end
+    
+    if self.image?
+      return "image"
+    else
+      return "unknown"
+    end
+  end
+
   def youtube_thumbnail(link)
       domain = link.split('/')[2]
-    
       if domain.include?("youtube")
         query_string = link.split("?").last
         if query_string
@@ -32,14 +59,15 @@ class Attachment < ActiveRecord::Base
           id = link.split('/').last
           return  "http://img.youtube.com/vi/#{id}/0.jpg"
       end
-
   end
 
   def thumbnail_url
-    if self.image?
+    if self.format? == "image"
       return attachment.image_url(:thumb).to_s
-    else
+    elsif self.format? == "youtube"
       return youtube_thumbnail(self.youtube_url).to_s
+    else        
+      return "logo.jpg"
     end
   end
 
